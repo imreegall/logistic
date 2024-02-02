1070df2cac2355774a0bd739c8d729c9d5d39fb6
imreegall <imreegall@gmail.com>
2024-02-02 17:36:36 +0300

    очистил проект от компонентов по умолчанию, настроил базовые настройки

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `
+              @import "./src/assets/styles/_mixins.sass"
+              @import "./src/assets/styles/_variables.sass"
+            `
+      }
+    }
+  }
 })
